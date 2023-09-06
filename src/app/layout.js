56cbb9bf5c854fde64ac8ebb95099ad95a7375e7
suraj <surajsht2055@gmail.com>
2023-09-06 06:27:24 +0545

    added dark mode functionality

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Open_Sans } from "next/font/google";
+import { Providers } from "./provider";
 
 const openSans = Open_Sans({ subsets: ["latin"] });
 
@@ -10,7 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={openSans.className}>{children}</body>
+      <body className={openSans.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
--- src/app/navbar/Navbar.jsx
+++ src/app/navbar/Navbar.jsx
@@ -1,20 +1,32 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import MenuIcon from "@mui/icons-material/Menu";
+import { useTheme } from "next-themes";
 
 const Navbar = () => {
   let [toggle, setToggle] = useState(false);
+  const { setTheme } = useTheme();
 
   let toggleTheme = () => {
     setToggle(!toggle);
   };
 
+  useEffect(() => {
+    if (toggle) {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  }, [toggle]);
+
   return (
     <div className="container px-4">
       <div className="flex items-center justify-between py-2">
-        <div className="text-2xl lg:text-4xl font-bold">NoodleNest</div>
+        <div className="text-2xl lg:text-4xl font-bold">
+          Noodle<span className="text-[#FF8800]">Nest</span>
+        </div>
 
         <ul className="hidden lg:flex lg:items-center lg:space-x-6">
           <li> Home </li>
@@ -26,11 +38,11 @@ const Navbar = () => {
 
         <div className="flex items-center gap-2">
           <div
-            className="relative h-5 w-11 border-[1px] border-[#26242e] rounded-xl cursor-pointer"
+            className="relative h-5 w-11 bg-[#fff] rounded-xl cursor-pointer"
             onClick={() => toggleTheme()}
           >
             <div
-              className={`absolute top-[50%] left-1 bottom-0 translate-y-[-50%] h-4 w-4 bg-black rounded-full ${
+              className={`absolute top-[50%] left-1 bottom-0 translate-y-[-50%] h-4 w-4 bg-[#000] rounded-full ${
                 toggle ? "translate-x-[18px]" : "translate-x-0"
               } transition duration-500`}
             ></div>
